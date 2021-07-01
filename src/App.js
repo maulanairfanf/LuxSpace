401d4bead33ed2e75dee76d3ec135d520f18ad37
maulanairfanf <maulanairfanf@gmail.com>
2021-07-01 08:53:13 +0700

    starteed react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import 'assets/css/app.css';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p class="text-purple-600 ">hai</p>
-
         <a
           className="App-link"
           href="https://reactjs.org"
